acf4cae3d05656193e70d139b0265831e65dc048
Jesse Quijano <jquijano@buyitinstalled.com>
2019-01-16 08:13:33 -0800

    remove redirection

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace JesseTest
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
