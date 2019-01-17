799b0047cfedc86cf10cf81a20566fdf523bb0cf
Jesse Quijano <jesse@quijano.net>
2019-01-17 09:20:42 -0800

    Define model to represent Log table.

---
+++ jtcontext/Models/Log.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace mysqlefcore
+{
+    [Table("Log")]
+    public class Log
+    {
+        public long LogId { get; set; }
+
+        public short LogLevel { get; set; }
+
+        [Column(TypeName = "text")]
+        public string SourcePage { get; set; }
+
+        [Column(TypeName = "text")]
+        public string SQL { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Message { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime Generated { get; set; }
+
+        public short Dispositioned { get; set; }
+
+        public DateTime? DispositionDate { get; set; }
+    }
+}
