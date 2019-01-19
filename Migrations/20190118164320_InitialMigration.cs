3debd774023361ea0eda7c20f36ac9a064b11065
Jesse Quijano <jquijano@buyitinstalled.com>
2019-01-18 20:43:43 -0800

    Add Log table

---
+++ Migrations/20190118164320_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JTracker.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Log",
+                columns: table => new
+                {
+                    LogId = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    LogLevel = table.Column<short>(nullable: false),
+                    SourcePage = table.Column<string>(type: "text", nullable: true),
+                    SQL = table.Column<string>(type: "text", nullable: true),
+                    Message = table.Column<string>(type: "text", nullable: true),
+                    Generated = table.Column<DateTime>(nullable: false),
+                    Dispositioned = table.Column<short>(nullable: false),
+                    DispositionDate = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Log", x => x.LogId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Log");
+        }
+    }
+}
---
+++ Migrations/20190118185805_LogTableUpdates.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JTracker.Migrations
+{
+    public partial class LogTableUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "LogLevel",
+                table: "Log",
+                nullable: false,
+                oldClrType: typeof(short));
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Dispositioned",
+                table: "Log",
+                nullable: false,
+                oldClrType: typeof(short));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "LogLevel",
+                table: "Log",
+                nullable: false,
+                oldClrType: typeof(byte));
+
+            migrationBuilder.AlterColumn<short>(
+                name: "Dispositioned",
+                table: "Log",
+                nullable: false,
+                oldClrType: typeof(byte));
+        }
+    }
+}
